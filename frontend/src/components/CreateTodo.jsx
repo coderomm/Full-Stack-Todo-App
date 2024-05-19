3788093bb7db1a555e1aa9bccf6fca682b9898d4
coder om <mail.coderom@gmail.com>
2024-05-19 23:24:39 +0530

    Create frontend

---
+++ frontend/src/components/CreateTodo.jsx
@@ -0,0 +1,18 @@
+export function CreateTodo() {
+    return <div>
+        <input style={{
+            padding: 10,
+            margin: 10
+        }} type="text" placeholder="Enter title" name="" id="" /> <br />
+
+        <input style={{
+            padding: 10,
+            margin: 10
+        }} type="text" placeholder="Enter description" name="" id="" /> <br />
+
+        <button style={{
+            padding: 10,
+            margin: 10
+        }}>Add a todo</button>
+    </div>
+}
---
+++ frontend/src/components/Todos.jsx
@@ -0,0 +1,7 @@
+export function Todos() {
+    return <div>
+        <h1>Go to gym</h1>
+        <h2>I am going to gym</h2>
+        <button>Mark as completed</button>
+    </div>
+}
